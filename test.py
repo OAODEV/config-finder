29271e5569a78b41a1b46e2196390a3e15fd3352
Jesse Miller <jesse.miller@adops.com>
2015-12-17 15:48:02 -0500

    adds failing test suite to watch circle fail

---
+++ test.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class TestConfigFinder(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_can_pass(self):
+        self.assertTrue(False)
